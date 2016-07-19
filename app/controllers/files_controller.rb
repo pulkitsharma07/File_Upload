0afcf523f93bdf7e4b9623749920bb278dd04bff
Pulkit Sharma <avastpulkit@gmail.com>
2016-07-19 16:39:02 +0530

    File Upload Functionality

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -5,11 +5,17 @@ class FilesController < ApplicationController
     upload = params[:upload]
     file_name = upload['datafile'].original_filename if upload['datafile'] != ''
     file = upload['datafile'].read
-    save_location = Rails.root.to_s
+    save_location = Rails.root.to_s + '/user_data'
 
-    File.open(save_location + '/user_data/' + file_name, "wb") do |f|
+    Dir.mkdir(save_location) unless File.directory?(save_location)
+
+    new_file_name = Digest::MD5.hexdigest(current_user.password_digest + file_name)
+
+    File.open(save_location + '/' + new_file_name, "wb") do |f|
             f.write(file)
     end
+
+    current_user.uploaded_files << UploadedFile.new(name: file_name, location: save_location + '/' + new_file_name)
 
     p "Uploaded : #{file_name}", file.size, current_user
     redirect_to '/'
