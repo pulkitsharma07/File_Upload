6366f3189d7abb2d1376aca4a1149f8a26271967
Pulkit Sharma <avastpulkit@gmail.com>
2016-07-20 14:05:26 +0530

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_many :uploaded_files
 
+  validates :name, presence: true, length: { minimum: 4, maximum: 10 }
+  validates :username, presence: true, uniqueness: true, length: { maximum: 10 }
+
   has_secure_password
+
+  validates :password, length: { minimum: 8 }
 end
